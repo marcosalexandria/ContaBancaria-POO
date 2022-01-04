1017f6e584f16fc345c0fbbd740b511ceeef912f
Marcos Alexandria <93044961+marcosalexandria@users.noreply.github.com>
2022-01-04 08:03:54 -0300

    Enunciado

---
+++ indes.js
@@ -0,0 +1,7 @@
+/*
+- Crie uma classe conta bancaria
+- Com as propiedades de saldo na conta corrente, saldo na conta poupança e juros da poupança
+- Crie os métodos de depósito e saque, também um método para tranferir dinheiro da corrente para a poupança
+- Além disso, crie uma conta especial que herda da conta normal
+- Na conta especial os juros são dobrados da conta normal
+*/
