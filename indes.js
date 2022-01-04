8a60d719c6bbe56999311d2806d44d0f5357ec68
Marcos Alexandria <93044961+marcosalexandria@users.noreply.github.com>
2022-01-04 08:34:56 -0300

    parte 1, conta normal e seus métodos.

--- indes.js
+++ indes.js
@@ -1,7 +1,38 @@
 /*
 - Crie uma classe conta bancaria
-- Com as propiedades de saldo na conta corrente, saldo na conta poupança e juros da poupança
-- Crie os métodos de depósito e saque, também um método para tranferir dinheiro da corrente para a poupança
+- Com as propiedades de saldo na conta corrente, saldo na conta poupança e juros da poupança(na data de aniversário, a conta poupança rende 1% do seu saldo)
+- Crie os métodos de depósito e saque, também um método para tranferir dinheiro da conta corrente para a poupança
 - Além disso, crie uma conta especial que herda da conta normal
 - Na conta especial os juros são dobrados da conta normal
 */
+
+class ContaBrancaria{
+    constructor(contaCorrente,contaPoupanca,jurosPoupanca){
+        this.contaCorrente=contaCorrente;
+        this.contaPoupanca=contaPoupanca;
+        this.jurosPoupanca=jurosPoupanca;
+    }
+    aniversario(){
+        this.contaPoupanca += this.contaPoupanca*this.jurosPoupanca/100;
+    }
+    deposito(valor){
+        this.contaPoupanca+=valor;
+    }
+    saque(valor){
+        if(valor>this.contaPoupanca){
+            console.log(`Você não tem o valor de R$: ${valor.toFixed(2)} na sua conta, verifique o seu saldo`);
+        }else{
+            this.contaPoupanca-=valor;
+        }
+    }
+    transferir(valor){
+        if(valor>this.contaCorrente){
+            console.log(`Você não tem o valor de R$: ${valor.toFixed(2)} na sua conta, verifique o seu saldo`);
+        }else{
+            this.contaCorrente-=valor;
+            this.contaPoupanca+=valor;
+        }
+    }
+}
+
+let conta= new ContaBrancaria(1000,1000,1);
